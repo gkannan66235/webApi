bede799e0c96e843b5e116d0eae7274fa5ad04fc
Kannan G <kannan.ganesan@thoughtworks.com>
2021-02-12 23:01:33 +0530

    splunk event collector added

--- Program.cs
+++ Program.cs
@@ -7,6 +7,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Serilog;
+using Serilog.Sinks.Splunk;
+using Serilog.Formatting.Compact;
+using Serilog.Sinks.AzureEventHub;
 
 namespace webApi
 {
@@ -16,7 +19,10 @@ namespace webApi
         {
             Log.Logger = new LoggerConfiguration()
                             .Enrich.FromLogContext()
-                            .WriteTo.Console()
+                            .WriteTo.Console(new RenderedCompactJsonFormatter())
+                            .WriteTo.Debug(outputTemplate: DateTime.Now.ToString())
+                            .WriteTo.EventCollector("http://localhost:8088/services/collector", "e8f47278-7ff0-4a33-a370-672afa4879b8")
+                            //.WriteTo.AzureEventHub("http://localhost:8088/services/collector", "e8f47278-7ff0-4a33-a370-672afa4879b8")
                             .CreateLogger();
             CreateHostBuilder(args).Build().Run();
         }
