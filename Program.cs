f1491655f119cdc54691120205edfad7ce482b3d
Kannan G <kannan.ganesan@thoughtworks.com>
2021-02-16 15:57:48 +0530

    Logging with template

--- Program.cs
+++ Program.cs
@@ -1,21 +1,48 @@
 using System;
+using System.IO;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Serilog;
-using Serilog.Formatting.Compact;
 
 namespace webApi
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static IConfiguration Configuration { get; } = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+                .AddEnvironmentVariables()
+                .Build();
+        public static int Main(string[] args)
         {
             Log.Logger = new LoggerConfiguration()
-                            .Enrich.FromLogContext()
-                            .WriteTo.Console(new RenderedCompactJsonFormatter())
-                            .WriteTo.EventCollector("http://localhost:8088/services/collector", "e8f47278-7ff0-4a33-a370-672afa4879b8")
-                            .CreateLogger();
-            CreateHostBuilder(args).Build().Run();
+                .ReadFrom.Configuration(Configuration)
+                .WriteTo.Console(
+                    outputTemplate: "[{Timestamp:HH:mm:ss} {CorrelationId} {Level:u3}] {Message:lj} {Properties:j}{NewLine}{Exception}")
+                .Enrich.WithCorrelationId()
+                .Enrich.FromLogContext()
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Getting the motors running...");
+                Log.Error("Getting the motors stopped...");
+
+                CreateHostBuilder(args).Build().Run();
+
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
